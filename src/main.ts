1e9050248b67e22817066b3de51a8e869974dc99
Gerardo Benitez <gerardo.benitez@livestories.com>
2025-08-04 23:23:03 -0300

    add swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+function getConfig(){
+  const config = new DocumentBuilder()
+    .setTitle('BlogPost API')
+    .setDescription('This API let create BlogPost and Comments for it')
+    .setVersion('1.0')
+    .build();
+  return config;
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = getConfig();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
